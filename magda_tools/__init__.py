39aca2f1c384a9725ab928ad6d607f9fac8be714
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2019-06-07 13:34:59 +0100

    Added noqa comments removed by black. Updated 17051_mrdcd_sdfgmc_ksm_1m.ffh to latest version from magda data.

--- magda_tools/__init__.py
+++ magda_tools/__init__.py
@@ -1,4 +1,4 @@
 from .calculate import distance  # noqa F401, F403
-from .calculate import SATURN_RADIUS_KM, latitude, saturn_local_time
+from .calculate import SATURN_RADIUS_KM, latitude, saturn_local_time  # noqa F401, F403
 from .data_file import DataFile  # noqa F401, F403
 from .header_handler import MAGDA_TIME_FMT, MAGDA_TIME_FMT_MS  # noqa F401, F403

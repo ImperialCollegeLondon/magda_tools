df2e1d926bef2de13f2150a1ea76de8b00afb199
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2019-11-23 11:45:13 +0000

    Removed references to header_handler from __init__.py

--- magda_tools/__init__.py
+++ magda_tools/__init__.py
@@ -1,5 +1,4 @@
 from .calculate import distance  # noqa F401, F403
 from .calculate import SATURN_RADIUS_KM, latitude, saturn_local_time  # noqa F401, F403
-from .data_file import DataFile  # noqa F401, F403
-from .header_handler import MAGDA_TIME_FMT, MAGDA_TIME_FMT_MS  # noqa F401, F403
+from .data_file import DataFile, MAGDA_TIME_FMT, MAGDA_TIME_FMT_MS  # noqa F401, F403
 from .model import BFieldModel  # noqa F401, F403

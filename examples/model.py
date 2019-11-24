7cf18a679983ae8b72292f4b7209b153f162a8a6
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2019-11-24 18:12:43 +0000

    Minor tweaks to model based example

--- examples/model.py
+++ examples/model.py
@@ -4,15 +4,16 @@ import matplotlib.pyplot as plt
 from magda_tools import DataFile, BFieldModel
 
 parser = argparse.ArgumentParser(
-    description="Plot the magnetic field strength data from a Magda flat file"
+    description="Plot the magnetic field strength from a Magda flat file predicted"
+    " by a three term harmonic expansion model"
 )
 parser.add_argument("FFD_file", type=str, help="Path to a Magda flat file")
 args = parser.parse_args()
 
-
+# initialise model with coefficients for expansion terms
 model = BFieldModel([21160.0, 1560.0, 2320.0])
-df = DataFile(args.FFD_file)
 
+df = DataFile(args.FFD_file)
 r, theta, phi = model.process_datafile(df)
 plt.plot(df["TIME"].data.datetime, r)
 plt.plot(df["TIME"].data.datetime, theta)

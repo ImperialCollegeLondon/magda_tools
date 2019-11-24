fe6da135a60b0257b50a544340d7e7ba1f9229fd
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2019-11-24 18:28:48 +0000

    Instated use of requirements.txt and requirements_dev.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
+from pathlib import Path
 from setuptools import setup
+
+base_path = Path(__file__).parent
+
+with open(base_path / "requirements.txt") as f:
+    requirements = f.readlines()
+
+with open(base_path / "requirements_dev.txt") as f:
+    requirements_dev = f.readlines()
 
 setup(
     name="magda_tools",
@@ -10,7 +19,7 @@ setup(
     author_email="",
     packages=["magda_tools"],
     python_requires=">=3.6",
-    install_requires=["astropy", "numpy", "sympy"],
-    tests_require=["pytz"],
+    install_requires=requirements,
+    tests_require=requirements_dev,
     project_urls={"Source": "https://github.com/ImperialCollege/magda_tools"},
 )

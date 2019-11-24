764ceed8d81dfd5309b7b282479b74facca89d81
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2019-11-24 18:44:42 +0000

    Added long description to setup.py

--- setup.py
+++ setup.py
@@ -9,11 +9,17 @@ with open(base_path / "requirements.txt") as f:
 with open(base_path / "requirements_dev.txt") as f:
     requirements_dev = f.readlines()
 
+description = """This package implements:
+1) A class for reading Magda data files in flat file (ffd) format
+2) Routines for calculating a handful of spacecraft position properties
+   with respect to Saturn
+3) An implementation of an arbitrary order B-field spherical harmonic expansion model"""
+
 setup(
     name="magda_tools",
     version="0.1",
     description="A collection of tools for working with MAGDA data files",
-    long_description="",
+    long_description=description,
     url="https://github.com/ImperialCollegeLondon/magda_tools",
     author="Research Software Engineering Group, Imperial College",
     author_email="",
